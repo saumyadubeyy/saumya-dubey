7e4a4ccf40c3fb051e7f5ccedd9ec019eb20e470
Saumya Dubey <saumyadb29@gmail.com>
2022-12-03 01:29:56 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
+// images
+import MainBannerImage from './assets/fleabag.jpg';
+// eslint-disable-next-line no-unused-vars
+import fightCImage from './assets/fightC.jpg'
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='bg-black-1 flex flex-col justify-center items-center md:max-h-screen min-h-screen gap-5 py-5'>
+      <p className='text-image-color-1 font-medium'>Coming soon.</p>
+      <div className='flex justify-center items-center overflow-hidden'>
+        <img src={MainBannerImage} alt='' />
+      </div>
+      <div className='flex flex-col items-center gap-1'>
+        <p className='text-image-color-2 font-medium opacity-60'>Saumya Dubey</p>
+        <div className='h-[2px] w-5 bg-image-color-2 opacity-60'></div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js}",
+    "./src/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    colors: {
+      'black-1': '#0B0B0B',
+      'gray-100': '#1F1F1F',
+      'gray-500': '#858585',
+      'white': '#FFFFFF',
+      'error': '#FF0000',
+      'image-color-1': '#695928',
+      'image-color-2': '#4C7666'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
